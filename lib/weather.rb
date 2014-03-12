3056174979fcf1cb4d3ddfdb1856813ef35f27fa
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-03-11 21:37:46 -0600

    Added the smallest spread output

--- lib/weather.rb
+++ lib/weather.rb
@@ -3,15 +3,35 @@ $LOAD_PATH.unshift(File.expand_path('lib'), __FILE__)
 class WeatherData
 
   def initialize
-    @weather_array = []
+    @full_file = []
+    @days_data = []
+    @spread_data = []
   end
 
 
   def read_file
     file = File.open('lib/weather_data.txt')
     file.readlines.each do |line|
-      @weather_array << line
+      @full_file << line
     end
-    @weather_array
+    @full_file
+  end
+
+  def get_data
+    @days_data = @full_file[8,30]
+  end
+
+  def find_smallest_spread
+    smallest_spread = 1000
+    @days_data.each do |row|
+      day_data = row.split(" ")
+      @spread_data << (day_data[1].to_i - day_data[2].to_i)
+    end
+    @spread_data.each do |spread|
+      if spread < smallest_spread
+        smallest_spread = spread
+      end
+    end
+    return "Day #{@spread_data.index(smallest_spread) + 1} has the smallest temperature spread at #{smallest_spread} degrees"
   end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -8,17 +8,26 @@ require 'weather'
 describe "Weather" do
   before do
     @data = WeatherData.new
-    @data = @data.read_file
+    @data1 = @data.read_file
+    @important_data = @data.get_data
+    @output = @data.find_smallest_spread
   end
   it "adds all of the lines of text to an array" do
 
 
-    expect(@data.count).to eq(40)
+    expect(@data1.count).to eq(40)
 
   end
 
-  it "there is a new array that only contains the 30 days of data" do
-    expect(@data.get_data).to eq(30)
-    
+  it "there is a new array that only contains the 30 rows of data" do
+
+    expect(@important_data.count).to eq(30)
+
+  end
+
+  it "outputs the day number with the smallest temperature spread" do
+
+    expect(@output).to eq("Day 14 has the smallest temperature spread at 2 degrees")
+
   end
 end
